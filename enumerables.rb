32d7741245c6f6de5bed48bcc90237070acbb0b6
ari karim <arikarim146@gmail.com>
2021-02-09 21:38:50 +0300

    added more powerful methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,53 +1,109 @@
 module Enumerable
   def my_each
-    length.times { |value| yield(self[value]) }
+    return to_enum(:my_each) unless block_given?
+
+    i = 0
+    while i < to_a.length
+      yield to_a[i]
+      i += 1
+    end
     self
   end
 
   def my_each_with_index
-    length.times { |value| yield(self[value], value) }
+    return to_enum(:my_each_with_index) unless block_given?
+
+    i = 0
+    while i < to_a.length
+      yield(to_a[i], i)
+      i += 1
+    end
     self
   end
 
   def my_select
-    array = []
-    my_each { |num| array << num if yield(num) }
-    array
+    return to_enum(:my_select) unless block_given?
+
+    new_arr = []
+    to_a.my_each { |item| new_arr << item if yield item }
+    new_arr
   end
 
-  def my_all?
-    result = true
-    my_each { |i| break result = false unless yield(i) }
-    result
+  def my_all?()
+    if block_given?
+      to_a.my_each { |item| return false if yield(item) == false }
+      return true
+    elsif param.nil?
+      to_a.my_each { |item| return false if item.nil? || item == false }
+    # elsif !param.nil? && (param.is_a? Class)
+    #   to_a.my_each { |item| return false unless [item.class, item.class.superclass].include?(param) }
+    # elsif !param.nil? && param.instance_of?(Regexp)
+    #   to_a.my_each { |item| return false unless param.match(item) }
+    else
+      to_a.my_each { |item| return false if item != param }
+    end
+    true
   end
 
-  def my_any?
-    result = false
-    my_each { |i| break result = true if yield(i) }
-    result
+  def my_any?(param = nil)
+    if block_given?
+      to_a.my_each { |item| return true if yield(item) }
+      return false
+    elsif param.nil?
+      to_a.my_each { |item| return true if item }
+    # elsif !param.nil? && (param.is_a? Class)
+    #   to_a.my_each { |item| return true if [item.class, item.class.superclass].include?(param) }
+    # elsif !param.nil? && param.class == Regexp
+    #   to_a.my_each { |item| return true if param.match(item) }
+    else
+      to_a.my_each { |item| return true if item == param }
+    end
+    false
   end
 
-  def my_none?
-    result = true
-    my_each { |i| break result = false if yield(i) }
-    result
+  def my_none?(param = nil)
+    if block_given?
+      !my_any?(&Proc.new)
+    else
+      !my_any?(param)
+    end
   end
 
-  def my_count(arg)
-    counter = 0
-    my_each { |i| counter += 1 if arg == i }
-    counter
+  def my_count(param = nil)
+    c = 0
+    if block_given?
+      to_a.my_each { |item| c += 1 if yield(item) }
+    elsif !block_given? && param.nil?
+      c = to_a.length
+    else
+      c = to_a.my_select { |item| item == param }.length
+    end
+    c
   end
 
-  def my_map(proc = nil)
-    array = []
-    block_given? ? my_each { |i| array << yield(i) } : my_each { |i| array << proc.call(i) }
-    array
+  def my_map(my_proc = nil)
+    return to_enum(:my_map) unless block_given? || !my_proc.nil?
+
+    arr = []
+    if proc.nil?
+      to_a.my_each { |item| arr << yield(item) }
+    else
+      to_a.my_each { |item| arr << proc.call(item) }
+    end
+    arr
   end
 
-  def my_inject(accumulator = 0)
-    my_each { |i| accumulator = yield(accumulator, i) }
-    accumulator
+  def my_inject(initial = nil, sym = nil)
+    if !initial.nil? && initial.is_a?(Symbol)
+      sym = initial
+      initial = nil
+    end
+    if !block_given?
+      to_a.my_each { |item| initial = initial.nil? ? item : initial.send(sym, item) }
+    else
+      to_a.my_each { |item| initial = initial.nil? ? item : yield(initial, item) }
+    end
+    initial
   end
 end
 
@@ -61,15 +117,15 @@ puts
 control = [1, 2, 8, 5, 5]
 puts "control array = #{control}"
 puts
-control.my_each { |value| print value.to_s + ' ' }
+control.my_each { |value| print " #{value} " }
 puts
-control.my_each_with_index { |value, ind| print ind.to_s + ':' + value.to_s + ' ' }
+control.my_each_with_index { |value, ind| print " #{ind}:#{value}  " }
 puts
 p(control.my_select { |num| num > 3 })
 p(control.my_all? { |i| i < 10 })
 p(control.my_any? { |i| i < 5 })
 p(control.my_none? { |value| value < 3 })
 p control.my_count(2)
-p control.my_map(my_proc) # { |i| i * 2 }
-p(control.my_inject { |sum, i| sum + i })
+p control.my_map(my_proc) { |i| i * 3 }
+p(control.my_inject { |sum, i| sum * i })
 p multiply_els(control)
